f20c035eb5295639ae6503d6ff177bbc6d937472
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2022-08-01 13:46:28 +0000

    deploy configuration changed

--- appstart.sh
+++ appstart.sh
@@ -1,1 +1,6 @@
-touch test
+#!/bin/bash
+cd /opt/one-tracker/
+sudo chmod -R 777 *
+pip3 install -r requirements.txt -U
+kill -9 $(ps -ef | grep "python3 /opt/one-tracker/one-tracker.py" | grep -v grep | awk '{print $2}')
+nohup python3 /opt/one-tracker/one-tracker.py > /dev/null 2> /dev/null < /dev/null &
