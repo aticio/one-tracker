34a6c3c947df67bc6579744b374355f0ec384f75
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2022-08-02 13:19:00 +0000

    poslist added

--- one-tracker.py
+++ one-tracker.py
@@ -9,6 +9,7 @@ EXCHANGE_INFO = "https://api.binance.com/api/v3/exchangeInfo"
 
 PRICE_DATA = {}
 WATCHLIST = []
+POSLIST = {}
 
 def main():
     exchange_info = get_exchange_info()
@@ -58,6 +59,7 @@ def on_open(w_s):
 def on_message(w_s, message):
     global PRICE_DATA
     global WATCHLIST
+    global POSLIST
     ticker_data = json.loads(message)
 
     for t in ticker_data:
@@ -66,13 +68,22 @@ def on_message(w_s, message):
             if len(PRICE_DATA[t["s"]]) == 3600:
                 PRICE_DATA[t["s"]].pop(0)
             PRICE_DATA[t["s"]].append(t["c"])
+
             if t['s'] not in WATCHLIST:
                 anomaly = check_anomaly(PRICE_DATA[t["s"]])
                 if anomaly:
+                    POSLIST[t['s']] = {'entry': float(t['c']), 'exit': 0.0}
                     WATCHLIST.append(t['s'])
                     print(time.ctime(), t["s"], t["c"])
                     f = open("anomaly.txt", "a")
                     f.write(time.ctime() + " - " + t["s"] + " - " + t["c"] + "\n")
+                    f.close()
+            
+            if POSLIST[t['s']] is not None:
+                if float(t['c']) > (POSLIST[t['s']]['entry'] + (POSLIST[t['s']]['entry'] * 0.01)):
+                    POSLIST[t['s']]['exit'] = float(t['c'])
+                    f = open("onepercent.txt", "a")
+                    f.write(time.ctime() + " - " + t["s"] + " - " + str(POSLIST[t['s']]['entry']) + " - " + str(POSLIST[t['s']]['exit']) + "\n")
                     f.close()
 
 
